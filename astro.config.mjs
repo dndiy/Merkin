ebc05aeefc3e4cc6d55f3eefc5241f73facc4ed9
Greg Aster <greg@dndiy.org>
2025-02-06 18:14:13 -0800

    failed upload try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dndiy.github.io',
+  site: 'https://merkin.github.io',
   base: '/',
   trailingSlash: "always",
   integrations: [
